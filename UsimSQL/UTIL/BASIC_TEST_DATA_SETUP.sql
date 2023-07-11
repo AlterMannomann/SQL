7cc54b293b221d9c2dbfa50799c6578b1106416b
Adi <toent@toent.ch>
2023-07-12 00:45:41 +0200

    View enriched with volume

--- UsimSQL/UTIL/BASIC_TEST_DATA_SETUP.sql
+++ UsimSQL/UTIL/BASIC_TEST_DATA_SETUP.sql
@@ -24,7 +24,7 @@ BEGIN
   l_usim_id_pos4 := usim_pos.insert_next_position(-1); -- -1, -1
   l_usim_id_vol := usim_vol.insert_vol(l_usim_id_mlv, l_usim_id_pos1, l_usim_id_pos2, l_usim_id_pos3, l_usim_id_pos4);
   l_usim_id_nod := usim_nod.insert_node;
-/*
+
   -- base rmd relation
   l_usim_id_rmd := usim_rmd.insert_rmd(l_usim_id_mlv, l_usim_id_dim);
   -- base rrpn relation
@@ -32,12 +32,26 @@ BEGIN
   -- next dimension 1
   l_usim_id_dim := usim_dim.insert_next_dimension;
   l_usim_id_rmd := usim_rmd.insert_rmd(l_usim_id_mlv, l_usim_id_dim);
-  l_usim_id_pos := usim_pos.insert_next_position(1); -- 1, positive sign
+  -- base 0 and 1
   l_usim_id_nod := usim_nod.insert_node;
-  l_usim_id_rrpn := usim_rrpn.insert_rrpn(l_usim_id_rmd, l_usim_id_pos, l_usim_id_nod);
-  l_usim_id_pos := usim_pos.insert_next_position(-1); -- -1, negative sign
+  l_usim_id_rrpn := usim_rrpn.insert_rrpn(l_usim_id_rmd, l_usim_id_pos1, l_usim_id_nod);
   l_usim_id_nod := usim_nod.insert_node;
-  l_usim_id_rrpn := usim_rrpn.insert_rrpn(l_usim_id_rmd, l_usim_id_pos, l_usim_id_nod);
-*/
+  l_usim_id_rrpn := usim_rrpn.insert_rrpn(l_usim_id_rmd, l_usim_id_pos2, l_usim_id_nod);
+  -- mirror 0 and -1
+  l_usim_id_nod := usim_nod.insert_node;
+  l_usim_id_rrpn := usim_rrpn.insert_rrpn(l_usim_id_rmd, l_usim_id_pos3, l_usim_id_nod);
+  l_usim_id_nod := usim_nod.insert_node;
+  l_usim_id_rrpn := usim_rrpn.insert_rrpn(l_usim_id_rmd, l_usim_id_pos4, l_usim_id_nod);
+
 END;
 /
+
+/* nice sort for view
+SELECT *
+  FROM usim_rrpn_v
+ ORDER BY usim_id_mlv
+        , usim_sign
+        , usim_coordinate
+        , usim_n_dimension
+;
+*/

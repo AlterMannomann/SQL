ca391051a7b62e46b5e49d408a6284084ea45739
AlterMannomann <toent@toent.ch>
2024-08-10 15:37:13 +0200

    Work on testing

--- UsimSQL/SH/run_usim_testing.sh
+++ UsimSQL/SH/run_usim_testing.sh
@@ -5,3 +5,8 @@ usim_log=/usim_src/SETUP/LOG/UsimTesting.log
 echo Current dir: "$(pwd)" > $usim_log
 echo User: "$(whoami)" >> $usim_log
 psql "options=--search_path=usim_test,public" -f /usim_src/SETUP/UsimTestExtension.psql 2>&1 | tee -a $usim_log
+# match regex can be used to split the tests or call special test setups, e.g. --match '^test_000|^setup_000'
+# default is that functions begin with test, setup, teardown, startup or shutdown
+pg_prove --dbname usim --schema usim_test --runtests -v 2>&1 | tee -a $usim_log
+# prepare usim_test to contain some data
+psql "options=--search_path=usim_test" -f /usim_src/SETUP/UsimTestPrepare.psql 2>&1 | tee -a $usim_log
